be3ea19aaa9eb8e278c6f36bcefbddcf8b977df4
archig97 <62295366+archig97@users.noreply.github.com>
2024-09-12 20:05:20 +0000

    changes to test files

--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -6,17 +6,23 @@ This module provides pytest cases to check our debugging.py function is working 
 
 from hw2_debugging import merge_sort
 
-
+"""
+First test case
+"""
 def test_1():
     input_arr = []
     expected_arr = []
     assert merge_sort(input_arr) == expected_arr
-
+"""
+Second test case
+"""
 def test_2():
     input_arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
     expected_arr = [1, 1, 2, 3, 3, 4, 5, 5, 6, 9]
     assert merge_sort(input_arr) == expected_arr
-
+"""
+Third test case
+"""
 def test_3():
     input_arr = [56, 23, 92, 78]
     expected_arr = [23, 56, 92, 92]
