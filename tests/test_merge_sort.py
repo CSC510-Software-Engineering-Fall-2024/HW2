535aa89d1a3dd3b49076c2572fa7520fa55ee5d1
Dharani-Guda <86072402+Dharani-Guda@users.noreply.github.com>
2024-09-12 16:35:04 +0000

    Test cases

--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -1,15 +1,19 @@
-"""
-Module to test merge sort functionality.
-"""
+import sys
+import os
+sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
 
+from hw2_debugging import merge_sort
 def test_1():
-    """Test case 1 for merge sort."""
-    return
+    input_arr = []
+    expected_arr = []
+    assert merge_sort(input_arr) == expected_arr
 
 def test_2():
-    """Test case 2 for merge sort."""
-    return
+    input_arr = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+    expected_arr = [1, 1, 2, 3, 3, 4, 5, 5, 6, 9]
+    assert merge_sort(input_arr) == expected_arr
 
 def test_3():
-    """Test case 3 for merge sort."""
-    return
+    input_arr = [56, 23, 92, 78]
+    expected_arr = [23, 56, 92, 92]
+    assert merge_sort(input_arr) == expected_arr
