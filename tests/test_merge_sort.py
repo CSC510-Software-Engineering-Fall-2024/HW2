a36884f336c7670a4ca57df01b17b30af177b91d
Dharani-Guda <86072402+Dharani-Guda@users.noreply.github.com>
2024-09-12 19:33:16 +0000

    Changes in workflow

--- tests/test_merge_sort.py
+++ tests/test_merge_sort.py
@@ -1,8 +1,5 @@
-import sys
-import os
-sys.path.insert(0,os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-
 from hw2_debugging import merge_sort
+
 def test_1():
     input_arr = []
     expected_arr = []
