df8a4cbb98985957e512359dd1bb1cf9ba613295
Dharani-Guda <86072402+Dharani-Guda@users.noreply.github.com>
2024-09-12 16:10:19 +0000

    Verifying bandit trace

--- rand.py
+++ rand.py
@@ -1,21 +1,6 @@
-"""
-This module contains a function to generate random numbers.
-"""
-
-import secrets
-
+import random
 
 def random_array(arr):
-    """
-    Replace elements of arr with cryptographically secure random numbers.
-
-    Args:
-        arr (list): A list of integers to be replaced with random values.
-
-    Returns:
-        list: The modified list with random integers.
-    """
     for i in range(len(arr)):
-        # Generates a secure random number between 1 and 20
-        arr[i] = secrets.randbelow(20) + 1
+        arr[i] = random.randint(1, 20)
     return arr
